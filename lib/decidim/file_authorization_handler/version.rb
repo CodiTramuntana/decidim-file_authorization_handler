36e7233b5a7036d14c2af8c5f17306a9344431e0
Nicole Solarz <nicole.so@coditramuntana.com>
2023-05-05 09:37:14 +0200

    Update Decidim to 0.27.2

--- lib/decidim/file_authorization_handler/version.rb
+++ lib/decidim/file_authorization_handler/version.rb
@@ -2,11 +2,11 @@
 
 module Decidim
   module FileAuthorizationHandler
-    DECIDIM_VERSION = "0.27.1"
+    DECIDIM_VERSION = "0.27.2"
 
     # Uses the latest matching Decidim version for
     # - major, minor and patch
     # - the optional extra number is related to this module's patches
-    VERSION = "#{DECIDIM_VERSION}.1".freeze
+    VERSION = "#{DECIDIM_VERSION}.0".freeze
   end
 end
